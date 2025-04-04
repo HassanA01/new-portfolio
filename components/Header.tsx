4542b5c9016f050884f7977c6d62c2153bfb8278
HassanA01 <aneeq.hassan@utoronto.ca>
2025-04-03 23:14:08 -0400

    cleaned repo and added header component

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+
+export const Header = () => {
+    return (
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Experience</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact
+            </a>
+
+        </nav>
+    )
+}
